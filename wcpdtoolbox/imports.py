3f2db5ef9167cc7868ef9179287a6958910eebcb
doucx <doucxldh@gmail.com>
2023-03-07 15:54:24 +0000

    新增工具

---
+++ wcpdtoolbox/imports.py
@@ -0,0 +1,13 @@
+# 导入需要的包 
+import cv2
+import torch
+import graphviz
+import gc
+
+from threading import Thread
+from multiprocess import Queue
+from fastcore import *
+from fastbook import *
+from fastcore.all import *
+import fastbook
+fastbook.setup_book()
